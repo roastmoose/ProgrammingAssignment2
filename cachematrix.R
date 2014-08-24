c0fc34a8437a756a7e760c76bf27188edcab37b9
roastmoose <zander.spray@gmail.com>
2014-08-24 14:14:05 -0500

    Inputting the assignment
    filling out functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions do what the assignment asked... seems straightforward...
+## makeCacheMatrix makes a matrix object that can cache its inverse
+## and cacheSolve computes the inverse or collects the cache if it's already solved
 
-## Write a short comment describing this function
+##Usage:
+##a=matrix(1:4,2,2)
+##obj<-makeCacheMatrix(a)
+##inv<-cacheSolve(obj)
+##
+##a%*%inv will return the identity matrix... tada!
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
 }
 
 
-## Write a short comment describing this function
+## Only thing to add to what was already said... requires 
+##calling library(MASS) for ginv for the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  library(MASS)
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- ginv(data, ...)
+  x$setinv(m)
+  m
 }
